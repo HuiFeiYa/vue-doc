672964066af8385ae6173e3349a08838d1ed8551
guanwanxiao <guanwanxiao@mbse.com.cn>
2024-02-23 10:10:32 +0800

    feat: vue 中 canvas 高清绘制

--- src/views/canvas.vue
+++ src/views/canvas.vue
@@ -30,11 +30,17 @@ const loadImage = (e) => {
     const [dx,dy,dw,dh] = aspectFit(offsetWidth, offsetHeight, offsetWidth,offsetHeight)
     width.value = offsetWidth
     height.value = offsetHeight
+    //  devicePixelRatio 返回当前显示设备的物理像素分辨率与CSS 像素分辨率之比。此值也可以解释为像素大小的比率：一个 CSS 像素的大小与一个物理像素的大小
+    const dpr = window.devicePixelRatio || 1;
+    console.log('dpr:',dpr)
+    // 默认画布的宽高是 300 * 150，在 style 中修改只是修改了展示，没有修改实际的 canvas 的大小
+    canvas.value.width = 600 * dpr
+    canvas.value.height = 300 * dpr
     ctx.clearRect(0,0,offsetWidth, offsetHeight)
-    // ctx.drawImage(img.value, 0,0, offsetWidth, offsetHeight)
-    // ctx.drawImage(img.value, 0,0, dw,dh-60)
-
-    ctx.drawImage(img.value, 0,0, dw,dh, 0,0,100,100)
+    // 设置设备像素比
+    ctx.scale(dpr,dpr)
+    const rate = 1
+    ctx.drawImage(img.value, 0,0, offsetWidth / rate, offsetHeight / rate)
 }
 
 
@@ -49,7 +55,8 @@ const loadImage = (e) => {
     <div>
         绘制:
         <!-- <canvas ref="canvas" style="width:474px;height: 204px; "></canvas> -->
-         <canvas :style="{width: width + 'px', height: height + 'px'}" ref="canvas" ></canvas>
+         <canvas ref="canvas" style="width: 600px;height: 300px;"></canvas>
+         <!-- <canvas ref="canvas" style="width: 300px;height: 150px;"></canvas> -->
     </div>
     <button @click="draw">绘制</button>
   </div>
