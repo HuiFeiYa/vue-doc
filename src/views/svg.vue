e1855a634181549095d0db13864fcd49806f7abe
guanwanxiao <guanwanxiao@mbse.com.cn>
2024-03-06 16:46:36 +0800

    part3 小人

--- src/views/svg.vue
+++ src/views/svg.vue
@@ -5,7 +5,18 @@
     <!-- <svg width="200" height="200" viewBox="0 0 50 50">
         <circle cx="0" cy="0" r="50" ></circle>
     </svg> -->
-
+    <p>part3</p>
+    <svg width="200" height="200" viewBox="0 0 200 200">
+        <circle cx="100" cy="40" r="30" fill="#cd803d" />
+        <line x1="40" y1="80" x2="160" y2="80" stroke="#cd803d" stroke-width="35" stroke-linecap="round"/>
+        <line x1="60" y1="160" x2="100" y2="80" stroke="#cd803d" stroke-width="35" stroke-linecap="round"/>
+        <line x1="140" y1="160" x2="100" y2="80" stroke="#cd803d" stroke-width="35" stroke-linecap="round"/>
+        <circle cx="100" cy="80" r="5" fill="black"></circle>
+        <circle cx="100" cy="100" r="5" fill="black"></circle>
+        <circle cx="85" cy="35" r="5" fill="white"></circle>
+        <circle cx="115" cy="35" r="5" fill="white"></circle>
+        <rect x="100" y="50" width="20" height="8" rx="2" fill="none" stroke="white" style="transform: translateX(-10px);"></rect>
+    </svg>
     <p>part2</p>
     <svg width="200" height="200" viewBox="0 0 200 200">
         <polygon points="100,0 0,30 200,30" fill="#234236"></polygon>
