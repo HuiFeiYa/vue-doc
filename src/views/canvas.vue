9aa1801f12db40edf0609507354f5cadc0e737b9
guanwanxiao <guanwanxiao@mbse.com.cn>
2024-03-11 19:20:28 +0800

    feat: 图片合成到 canvas 上

--- src/views/canvas.vue
+++ src/views/canvas.vue
@@ -24,25 +24,16 @@ const aspectFit = (imageWidth, imageHeight, canvasWidth, canvasHeight) => {
   return [dx, dy, dw, dh]
 }
 
-const loadImage1 = (e) => {
-    const ctx = canvas.value.getContext('2d')
-    const {offsetWidth, offsetHeight } = img.value
-    const [dx,dy,dw,dh] = aspectFit(offsetWidth, offsetHeight, offsetWidth,offsetHeight)
-    width.value = offsetWidth
-    height.value = offsetHeight
-    //  devicePixelRatio 返回当前显示设备的物理像素分辨率与CSS 像素分辨率之比。此值也可以解释为像素大小的比率：一个 CSS 像素的大小与一个物理像素的大小
-    const dpr = window.devicePixelRatio || 1;
-    console.log('dpr:',dpr)
-    // 默认画布的宽高是 300 * 150，在 style 中修改只是修改了展示，没有修改实际的 canvas 的大小
-    canvas.value.width = 600 * dpr
-    canvas.value.height = 300 * dpr
-    ctx.clearRect(0,0,offsetWidth, offsetHeight)
-    // 设置设备像素比
-    ctx.scale(dpr,dpr)
-    const rate = 1
-    ctx.drawImage(img.value, 0,0, offsetWidth / rate, offsetHeight / rate)
+const addImage = (path) => {
+  const img = new Image()
+  return new Promise((resolve, reject)=> {
+    img.onload = ()=> {
+      resolve(img)
+    }
+    img.src = path
+  })
 }
-const loadImage = (e) => {
+const loadImage = async (e) => {
     const ctx = canvas.value.getContext('2d')
     const {offsetWidth, offsetHeight } = img.value
     const [dx,dy,dw,dh] = aspectFit(offsetWidth, offsetHeight, offsetWidth,offsetHeight)
@@ -52,15 +43,19 @@ const loadImage = (e) => {
     const dpr = window.devicePixelRatio || 1;
     console.log('dpr:',dpr)
     // 默认画布的宽高是 300 * 150，在 style 中修改只是修改了展示，没有修改实际的 canvas 的大小
-    canvas.value.width = 600 * dpr
-    canvas.value.height = 300 * dpr
+    canvas.value.width = offsetWidth * dpr
+    canvas.value.height = offsetHeight * dpr
     ctx.clearRect(0,0,offsetWidth, offsetHeight)
     // 设置设备像素比
     ctx.scale(dpr,dpr)
-    const rate = 1
-    const clipWidth = 100
-    const clipHeight = 100
-    ctx.drawImage(img.value, 0,0, 90,90, 0,0, clipWidth,clipHeight)
+    ctx.drawImage(img.value, 0, 0, offsetWidth, offsetHeight)
+    const shuiguoImg = await addImage('../public/shuiguo.png')
+    const { width:sgWidth, height:sgHeight } = shuiguoImg
+    const targetWidth = 100
+    const rate = sgWidth / sgHeight
+    const targetHeight = targetWidth / rate
+    ctx.drawImage(shuiguoImg, 0, 0, targetWidth, targetHeight)
+
 }
 
 
@@ -71,12 +66,12 @@ const loadImage = (e) => {
     <div>
         源:
         <!-- <img  ref="img" src="../assets/河马.png" alt="" @load="loadImage"> -->
-        <img  ref="img" src="../../public/avator.jpg" alt="" @load="loadImage">
+        <img style="width:200px"  ref="img" src="../../public/dog.png" alt="" @load="loadImage">
     </div>
     <div>
         绘制:
         <!-- <canvas ref="canvas" style="width:474px;height: 204px; "></canvas> -->
-         <canvas ref="canvas" style="width: 600px;height: 300px;"></canvas>
+         <canvas ref="canvas" :style="{width: width + 'px',height: height + 'px'}"></canvas>
          <!-- <canvas ref="canvas" style="width: 300px;height: 150px;"></canvas> -->
     </div>
     <button @click="draw">绘制</button>
